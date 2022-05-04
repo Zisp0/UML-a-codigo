7d18a6c5d49a3fc2f4b343f234a2bc6e84239043
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2022-05-04 14:37:47 -0500

    Update código.java

--- Composición y agregación/código.java
+++ Composición y agregación/código.java
@@ -1,1 +1,17 @@
-aa
+class Wheel {
+    private String manufacturer;     
+}
+
+class Brand {
+    private String name;
+}
+  
+final class Car {
+    private final Wheel wheel; // Composición
+    private Brand brand; // Agregación
+         
+    Car(Wheel wheel, Brand brand) {
+        this.wheel = wheel;
+        this.brand = brand;
+    }
+}
