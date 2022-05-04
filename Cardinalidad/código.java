e08547a7388bb6c9c96fb01ea12166d1e23e2160
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2022-05-04 14:25:29 -0500

    Update and rename Nuevo documento de texto.txt to código.java

---
+++ Cardinalidad/código.java
@@ -0,0 +1,12 @@
+public class University {
+    private String universityId;
+    private String universityName;
+    private String address;
+    private List<College> colleges;
+}
+
+public class College {
+    private String collegeId;
+    private String collegeName;
+    private University university;
+}
